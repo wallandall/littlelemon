1fe5f2ed517c5fda9ada2958adabebf7fb660d6b
wallandall <wallandall@gmail.com>
2024-03-10 18:39:49 +0100

    week 2 updates

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "littlelemon",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
